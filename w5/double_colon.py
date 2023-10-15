a174cda119b1545904c79e2d3ca2dbc3d214558f
Rory <chengithub77@gmail.com>
2023-10-16 04:09:02 +0800

    w5: Exercise

---
+++ w5/double_colon.py
@@ -0,0 +1,17 @@
+# collection[start:stop:step]
+
+num = [2, 4, 6, 8, 10]
+
+# start 2
+print(num[2:])
+
+# stop 2
+# print(num[:2])
+
+# step 2
+# print(num[::2])
+
+
+# print(num[-1])
+# print(num[-1::2])
+# print(num[-1::-2])
