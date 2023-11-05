643d97dc2e6305ee19dd77d611131ac18939209b
Rory <chengithub77@gmail.com>
2023-11-05 23:34:31 +0800

    mid exam

---
+++ mid/mid_ans.py
@@ -0,0 +1,15 @@
+def print_info():
+    from datetime import datetime
+
+    """
+    Display personal info.
+
+    score:
+        Name(str): 10%
+        Id(int): 10%
+        Date(str): use function 10%, if not 5%
+    """
+    print("陳冠宇", 611460162, datetime.now())
+
+
+card = {"type": "Visa", "number": 4195028642902074, "Exp. data": "03/2026", "CVV": 727}
