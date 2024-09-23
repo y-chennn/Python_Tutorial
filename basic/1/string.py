c88d8261feb2999c43cfc4ddb88eff2bb219e8f9
rory/rvl <chengithub77>
2024-09-23 17:15:57 +0800

    week 2 update

---
+++ basic/1/string.py
@@ -0,0 +1,10 @@
+s1 = "hello world"
+s2 = "hello world!   "
+
+print(s1, s2.strip())
+print(len(s1), len(s2))
+
+s3 = "hello, world"
+print(s3, s3.replace(",", " "))
+
+print(s3.upper(), s3.lower())
